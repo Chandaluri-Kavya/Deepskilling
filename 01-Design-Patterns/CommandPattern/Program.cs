2928acb01af308f646b0d4ef6d9e97ed60952475
Chandaluri-Kavya <chandalurikavya@gmail.com>
2026-06-30 09:14:56 +0530

    Completed Command Pattern

---
+++ 01-Design-Patterns/CommandPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿Light light = new Light();
+
+RemoteControl remote = new RemoteControl();
+
+remote.SetCommand(new LightOnCommand(light));
+remote.PressButton();
+
+remote.SetCommand(new LightOffCommand(light));
+remote.PressButton();
