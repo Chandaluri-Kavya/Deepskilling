7f4a1df95a9387b7d09b0e291fbdacb79405647e
Chandaluri-Kavya <chandalurikavya@gmail.com>
2026-07-05 17:44:11 +0530

    Completed Financial Forecasting

---
+++ 02-Data-Structures-And-Algorithms/FinancialForecasting/FinancialForecast.cs
@@ -0,0 +1,10 @@
+public class FinancialForecast
+{
+    public static double PredictFutureValue(double presentValue, double growthRate, int years)
+    {
+        if (years == 0)
+            return presentValue;
+
+        return PredictFutureValue(presentValue * (1 + growthRate), growthRate, years - 1);
+    }
+}
---
+++ 02-Data-Structures-And-Algorithms/FinancialForecasting/Program.cs
@@ -0,0 +1,7 @@
+﻿double presentValue = 10000;
+double growthRate = 0.10;
+int years = 5;
+
+double futureValue = FinancialForecast.PredictFutureValue(presentValue, growthRate, years);
+
+Console.WriteLine($"Future Value after {years} years = {futureValue:F2}");
