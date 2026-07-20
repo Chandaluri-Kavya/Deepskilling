0873b09d09a5b8919baa57efb5b75054482ee1ca
Chandaluri-Kavya <chandalurikavya@gmail.com>
2026-07-20 19:38:58 +0530

    Added EF Core HOL1

---
+++ 05-EntityFrameworkCore/EFCore-HOL1/RetailInventory/Program.cs
@@ -0,0 +1,26 @@
+﻿using RetailInventory.Data;
+using RetailInventory.Models;
+
+using var context = new AppDbContext();
+
+// Create Category
+var category = new Category
+{
+    Name = "Electronics"
+};
+
+context.Categories.Add(category);
+context.SaveChanges();
+
+// Create Product
+var product = new Product
+{
+    Name = "Laptop",
+    Price = 75000,
+    CategoryId = category.Id
+};
+
+context.Products.Add(product);
+context.SaveChanges();
+
+Console.WriteLine("Category and Product inserted successfully.");
